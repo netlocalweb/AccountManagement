5d7fb13a938072c6b6066b1efbdb48215f8e002b
marjotoska <mariotoska13@gmail.com>
2022-05-17 20:59:55 +0200

    User, UserDto classes added, TestController class modified, yt video tut completed

--- AccountManagement/Controllers/TestController.cs
+++ AccountManagement/Controllers/TestController.cs
@@ -1,15 +1,112 @@
 ﻿using Contracts;
+
+//my code
+using Entities.Models;
+using Entities.DTO;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using System.Collections.Generic;
+using Microsoft.IdentityModel.Tokens;
+//my code - END
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System;
 
 namespace AccountManagement.Controllers
 {
+
     [Route("api/test/[action]")]
     [ApiController]
     public class TestController : ControllerBase
     {
+
+        //my code
+
+        public static User user = new User();
+
+        private readonly IConfiguration _configuration;
+        public TestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.Username != request.Username) 
+            {
+                return BadRequest("User not found!"); 
+            }
+            if(!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong Password");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(.5),
+                signingCredentials: credentials
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        //my code - END
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
         private readonly IDapperRepository _dapperRepository;
+        
 
         public TestController(IRepositoryManager repository, ILoggerManager logger, IDapperRepository dapperRepository)
         {
--- AccountManagement/Program.cs
+++ AccountManagement/Program.cs
@@ -13,6 +13,7 @@ namespace AccountManagement
     {
         public static void Main(string[] args)
         {
+
             CreateHostBuilder(args).Build().Run();
         }
 
